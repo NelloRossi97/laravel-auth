3cefaed307e6dd763a4083fc88941c6292ed81b3
NelloRossi <rossianiello97@gmail.com>
2023-06-05 14:51:19 +0200

    added projects database

---
+++ config/projects.php
@@ -0,0 +1,43 @@
+<?php
+return $projects = [
+    [
+        'title' => 'laravel-auth',
+        'description' => 'A website that allow you to open the admin dashboard if you are logged in using Laravel'
+    ],
+    [
+        'title' => 'laravel-dc-comics',
+        'description' => 'A website that allow you to see a list of DC comics using Laravel'
+    ],
+    [
+        'title' => 'laravel-migration-seeder',
+        'description' => 'A website that allow you to migrate and seed a database using Laravel'
+    ],
+    [
+        'title' => 'laravel-model-controller',
+        'description' => 'A website that allow you to create a Model and a Controller using Laravel'
+    ],
+    [
+        'title' => 'laravel-primi-passi',
+        'description' => 'A website that allow you to join the world of Laravel'
+    ],
+    [
+        'title' => 'laravel-comics',
+        'description' => 'A website that allow you to see a list of comics using Laravel'
+    ],
+    [
+        'title' => 'php-hotel',
+        'description' => 'A website that allow you to see a list of Hotels using PHP'
+    ],
+    [
+        'title' => 'php-badwords',
+        'description' => 'A website that allow you to censor badwords using PHP'
+    ],
+    [
+        'title' => 'vite-boolflix',
+        'description' => 'A website that show you a list of films (Netflix style) using Vite'
+    ],
+    [
+        'title' => 'vite-yu-gi-oh',
+        'description' => 'A website that allow you to see You-Gi-Oh cards using Vite'
+    ],
+];
